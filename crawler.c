2bc2b152d42d6420e459912b87f5cb5e25a4487d
FilippoFinotto <filippogsishdhs@gmail.com>
2025-08-02 00:20:36 +0200

    commit pulizia finale

--- crawler.c
+++ crawler.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 
+#include "queue.h"
+
+
+
+
 int main(){
+
+	
+
+
+
 	return 0;
 }
--- queue.h
+++ queue.h
@@ -6,9 +6,6 @@
 #include <string.h>
 
 
-
-
-
 typedef struct Node{
         char *url;
         struct Node *next;
@@ -20,9 +17,9 @@ typedef struct Queue{
 }Queue;
 
 Queue* create_queue();
-void enqueue(Queue* q, const char* url);
-char* dequeue(Queue* q);
-int is_empty(Queue* q);
-void destroy_queue(Queue* q);
+void enqueue(Queue *q, const char *url);
+char* dequeue(Queue *q);
+int is_empty(Queue *q);
+void destroy_queue(Queue *q);
 
 #endif
